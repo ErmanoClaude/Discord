0c7be7af93407106494a51c155359a87e0e6ebb8
Ermano <ermano.claude@cix.csi.cuny.edu>
2023-08-27 18:44:04 -0400

    Boilerplate of client and sever created

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState } from 'react'
+
+const App = () => {
+
+  const [backendData, setBackendData] = useState([{}]);
+  
+  return (
+    <div>
+      <h1>HEllo FROM discord</h1>
+    </div>
+  )
+}
+
+export default App;
