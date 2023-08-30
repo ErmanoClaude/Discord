b8e2d3c330a1dac79bb23082336859a8dd8a9001
Ermano <ermano.claude@cix.csi.cuny.edu>
2023-08-30 18:10:35 -0400

    updating login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,26 +1,24 @@
-import '../pagesCSS/login.css'
+import "../pagesCSS/login.css"
 
 function Login() {
 
     return (
-        <div className="login">
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
+        <body>
+            <link rel="stylesheet" href="../pagesCSS/login.css"/>
+            <div className="login">
+                <form clasName="login-card">
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </body>
     );
 };
 
