bdc06a1591a0eb80034d0c5364663b819185a167
Ermano <ermano.claude@cix.csi.cuny.edu>
2023-11-28 17:54:56 -0500

    changed to aws db

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -2,15 +2,17 @@ const mysql = require("mysql");
 const env = require("dotenv").config();
 
 // AWS database connection
-/*
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-})
-*/
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: "discord_database",
+});
+
 // local connection to the MySQL WorkBench server
+/*
 const db = mysql.createConnection({
   host: "localhost", // The hostname of your MySQL server
   user: "root", // Your MySQL username
@@ -18,5 +20,5 @@ const db = mysql.createConnection({
   database: "discord_database", // The name of your database
 });
 // connect to database first then set server routes.
-
+*/
 module.exports = db;
